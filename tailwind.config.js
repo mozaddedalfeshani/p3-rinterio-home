bde0692b3fb03dac5dd252da0dc3b61277833b1c
M A Murad <72880355+mozaddedalfeshani@users.noreply.github.com>
2024-08-21 23:25:09 +0600

    No change to problem with nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greyLow: "#737373",
+      },
+      fontFamily: {
+        manrop: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
